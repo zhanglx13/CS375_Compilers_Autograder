01a59043c1b75602b9c378dcd6d43f6f4603e1f5
Lixun Zhang <zhanglx@utexas.edu>
2020-02-12 12:54:54 -0600

    change the server from linux to aggravation

--- toServer.sh
+++ toServer.sh
@@ -2,11 +2,11 @@
 
 ##
 ## $1 is the file/folder to copy to
-## lxzhang@linux.cs.utexas.edu:/u/lxzhang/$2/
+## lxzhang@aggravation.cs.utexas.edu:/u/lxzhang/$2/
 ##
 
 if [[ -d $1 ]]; then
-	scp -r $1 lxzhang@linux.cs.utexas.edu:/u/lxzhang/$2/
+	scp -r $1 lxzhang@aggravation.cs.utexas.edu:/u/lxzhang/$2/
 else
-	scp $1 lxzhang@linux.cs.utexas.edu:/u/lxzhang/$2/
+	scp $1 lxzhang@aggravation.cs.utexas.edu:/u/lxzhang/$2/
 fi
