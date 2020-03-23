e5c00e3527c0a9757d96389d8e3a8bcaaee0c4f6
Lixun <zhanglx@utexas.edu>
2020-03-22 22:54:25 -0500

    [p1-autograder] Fix all false alarms
    Features
    
    - check if seg fault happens
    - ignore overflow error message content
    - ignore token printouts if the token overflows
    - check if error message is missing
    - ignore the 6th digit after decimal
    
    Note that this is not fully tested so it might require further debugging
    next semester.

--- lexer_autograder.sh
+++ lexer_autograder.sh
@@ -31,10 +31,6 @@
 
 # for entry in ./test_p1/*
 # do
-#     ##
-#     ## Get file name only without path nor extension
-#     ## https://stackoverflow.com/questions/3362920/get-just-the-filename-from-a-path-in-a-bash-script
-#     ##
 #     xpath=${entry%/*}
 #     xbase=${entry##*/}
 #     xfext=${xbase##*.}
@@ -50,10 +46,6 @@
 
 # for entry in ./test_p1/*
 # do
-#     ##
-#     ## Get file name only without path nor extension
-#     ## https://stackoverflow.com/questions/3362920/get-just-the-filename-from-a-path-in-a-bash-script
-#     ##
 #     xpath=${entry%/*}
 #     xbase=${entry##*/}
 #     xfext=${xbase##*.}
@@ -72,30 +64,215 @@ elif [[ $1 == "p2" ]];then
     EXE=lexer
 fi
 
+
+
+##
+## Global array defining overflow cases
+##
+declare -A sArray
+sArray=(
+    [20]=1
+    [21]=1
+    [50]=1
+    [52]=1
+    [53]=1
+)
+##
+## This function checks the special cases of p1
+##
+## 1. Integer overflow: scantst_20, 21
+## 2. Floating overflow: scantst_50, 52, 53
+## 3. Floating number 3.141592ex vs 3.141593ex
+##
+## Input:
+##   $1 testname, in the form of scantst_xx or trivb_1
+##   $2 student output file
+##   $3 sample output file
+##
+checkSpecial()
+{
+    cmp=0
+    ##
+    ## Get the test case number, pay attention to trivb_1
+    ##
+    if [[ $1 == "trivb_1" ]]; then
+        num=0
+    else
+        num=${1:8}
+    fi
+    ##
+    ## Checks if the sample output contains scientific numbers
+    ##
+    cnt=$(egrep -c '*[0-9]e[+|-]*' $3)
+    if [[ ${sArray[$num]} ]]; then
+        ##
+        ## Overflow special cases
+        ##
+        ##
+        ## First, get the error message
+        ##
+        errMsg=$(egrep -v tokentype $2 | egrep -v Started)
+        if [[ $errMsg == "" ]]; then
+            ##
+            ## If overflow message not found, then print error
+            ##
+            echo ""
+            echo "$1"
+            echo ""
+            echo "=> Empty error message!!!"
+            ((wronglines++))
+            ##
+            ## Note that for scantst_20, there are three
+            ## more lines after the overflowed integer.
+            ## However, we do not check them since we already
+            ## found the error message missing.
+            ##
+        else
+            ##
+            ## If error message exists, then remove lines
+            ## relevant to the overflow error
+            ##
+            ## grep tokentype lines and remove the first
+            ##
+            egrep 'tokentype' $2 | awk 'NR>1' > result_tmp
+            egrep 'tokentype' $3 | awk 'NR>1' > sample_tmp
+            DIFF=$(diff result_tmp sample_tmp)
+            if [[ $DIFF != "" ]]; then
+                echo ""
+                echo "$1"
+                echo ""
+                echo "$DIFF"
+                ((wronglines++))
+            fi
+        fi
+    elif [[ $cnt -gt 0 ]]; then
+        ##
+        ## The sample of this case has one and only one
+        ## scientific number
+        ##
+        ## First check if student's output also has
+        ## only one scientific number
+        ##
+        myCnt=$(egrep -c '*[0-9]e[+|-]*' $2)
+        if [[ $myCnt -ne $cnt ]];then
+            cmp=1
+        else
+            ##
+            ## Then we should take a close look at the
+            ## line containing the scientific number
+            ##
+            egrep '*[0-9]e[+|-]*' $3 > sampleLine
+            egrep '*[0-9]e[+|-]*' $2 > myLine
+            ##
+            ## Get tokentype, type, mantissa, and exponent
+            ##
+            ## Note that we need to handle mantissa carefully
+            ## since it is allowed to have an error less than 0.000001
+            ##
+            ## For the rest fields, we just compare them as a string
+            ## since we need exact match for those fields
+            my_mantissa=$(awk '{print $5}' myLine | awk -Fe '{print $1}')
+            myOutput="$(cat myLine)"
+            myOutput=${myOutput/$my_mantissa/}
+            sample_mantissa=$(awk '{print $5}' sampleLine | awk -Fe '{print $1}')
+            sampleOutput="$(cat sampleLine)"
+            sampleOutput=${sampleOutput/$sample_mantissa/}
+            ##
+            ## Compare each component
+            ##
+            mantissa_err=$(echo "$sample_mantissa - $my_mantissa" | bc)
+            if [[ $(echo "$mantissa_err > 0.000001 || $mantissa_err < -0.000001" | bc) -eq 1 ]];then
+                cmp=1
+            fi
+            if [[ "$myOutput" != "$sampleOutput" ]]; then
+                cmp=1
+            fi
+        fi
+    else
+        ##
+        ## Other cases, just compare the outputs
+        ##
+        cmp=1
+    fi
+    ##
+    ## Normal compare
+    ##
+    if [[ $cmp -eq 1 ]]; then
+        DIFF=$(diff $2 $3)
+        if [[ $DIFF != "" ]]; then
+            echo ""
+            echo "$1"
+            echo ""
+            echo "$DIFF"
+            ((wronglines++))
+        fi
+    fi
+    ##
+    ## Clean up
+    ##
+    rm -f result_tmp sample_tmp myLine sampleLine
+}
+
+
 gradeSingleStudent()
 {
     echo "######################  $WHO  #########################"
     wronglines=0
     make $EXE &> compilation_dump
     if [[ -f "$EXE" ]];then
+        echo ">>>>>>>>>>"
         echo "scantst:"
+        echo ">>>>>>>>>>"
         for testInput in $TESTS
         do
             xbase=${testInput##*/}
             xpref=${xbase%.*}
-            ./$EXE < $testInput &> result
-            DIFF=$(diff result $SAMPLEDIR/$xpref.sample)
-            if [ "$DIFF" != "" ]
-            then
+            ##
+            ## Run the lexer and redirect stdout and stderr to result
+            ##
+            ## Note that if the program seg faults, the signal received
+            ## by the program is redirected to result. But the signal
+            ## received by bash is not. Therefore, we need to use Msg
+            ## to wrap the signal message
+            ##
+            Msg=$(./$EXE < $testInput &> result)
+            ##
+            ## Then we check if the exit code of the last command
+            ## is 139, which is SIGNAL 11, i.e. segmentation fault
+            ## If so, we simply print segfault for this test
+            ##
+            if [[ $? -eq 139 ]]; then
+                ##
+                ## Seg fault
+                ##
+                echo ""
                 echo "$xpref"
-                echo "$DIFF"
+                echo ""
+                echo "=> Seg fault"
                 ((wronglines++))
+            else
+                DIFF=$(diff result $SAMPLEDIR/$xpref.sample)
+                if [ "$DIFF" != "" ]
+                then
+                    if [[ $EXE -eq "lexanc" ]]; then
+                        checkSpecial $xpref result $SAMPLEDIR/$xpref.sample
+                    else
+                        echo ""
+                        echo "$xpref"
+                        echo ""
+                        echo "$DIFF"
+                        ((wronglines++))
+                    fi
+                fi
             fi
         done
         ##
         ## The wronglines variable makes little sense because it contains several
         ## false alert:
         ## 1. overflow message
+        ##    scantst 20, 21 (int overflow) 50, 52, 53 (floating overflow)
+        ##    case a: number value does not match
+        ##    case b: error message does not match
         ## 2. the 6th digit in the mantissa
         ##
         ## I do not have plans to exclude these false alert in the near future.
@@ -113,18 +290,28 @@ gradeSingleStudent()
         ## as the DIFF for the last test case of scantst.pas, which is scantst_9
         ##
         DIFF=""
+        echo ""
+        echo ">>>>>>>>>>"
         echo "graph1:"
-	    ./$EXE < $FILEDIR/graph1.pas &> result
-        if [[ $1 == "p1" ]]; then
-            DIFF=$(diff result graph1.lex)
-        elif [[ $1 == "p2" ]]; then
-            DIFF=$(diff result graph1.lexer)
-        fi
-        if [ "$DIFF" != "" ]
-        then
-            echo "$DIFF"
+        echo ">>>>>>>>>>"
+	    Msg=$(./$EXE < $FILEDIR/graph1.pas &> result)
+        ##
+        ## Check if the last command seg faults
+        ##
+        if [[ $? -eq 139 ]]; then
+            echo "=> Seg fault"
         else
-            echo -e "\xE2\x9C\x94"
+            if [[ $1 == "p1" ]]; then
+                DIFF=$(diff result graph1.lex)
+            elif [[ $1 == "p2" ]]; then
+                DIFF=$(diff result graph1.lexer)
+            fi
+            if [ "$DIFF" != "" ]
+            then
+                echo "$DIFF"
+            else
+                echo -e "\xE2\x9C\x94"
+            fi
         fi
         rm *.o result $EXE
     else
