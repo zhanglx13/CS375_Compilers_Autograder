1b956532516fe602a21986d1077b2d1da0529314
Lixun <zhanglx@utexas.edu>
2020-05-22 12:23:01 -0500

    Add a universal autograder for all projects

---
+++ grade.sh
@@ -0,0 +1,68 @@
+#! /usr/bin/env bash
+
+set -e
+
+printUsage()
+{
+    echo "Usage: ./grade.sh -p n -d dir"
+    echo "  n:   project number starting from 1 to 6"
+    echo "  dir: working dir"
+}
+
+OPTIND=1
+
+pn=0
+WD=""
+while getopts "p:d:h" opt; do
+    case "$opt" in
+        h)
+            printUsage
+            exit 0
+            ;;
+        p)
+            pn=$OPTARG
+            ;;
+        d)
+            WD=$OPTARG
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+##
+## Check project number
+##
+if [[ $pn -lt 1 ]] || [[ $pn -gt 6 ]]; then
+    echo "project number must be in range [1,6]"
+    printUsage
+    exit 0
+fi
+
+##
+## check working dir
+##
+if [[ $WD == "" ]]; then
+    echo "Must specify a working dir"
+    printUsage
+    exit 0
+fi
+
+##
+## Now everything is within range, run the autograder
+##
+if [[ $pn -lt 3 ]]; then
+    ./lexer_autograder.sh p$pn $WD
+elif [[ $pn -lt 6 ]]; then
+    ./parser_autograder.sh p$pn $WD
+else
+    ./codegen_autograder.sh $WD
+fi
+
+
