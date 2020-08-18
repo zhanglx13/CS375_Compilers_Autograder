c017e09b72d6578b8ea47d207ffd81c8a1c177ac
Lixun Zhang <zhanglx@utexas.edu>
2020-08-18 16:21:56 -0500

    [p6] Remake test cases

--- test_p6/test13.pas
+++ test_p6/test13.pas
@@ -1,18 +1,7 @@
 program graph1(output);
-label 1492, 1776;
-type complex = record re, im: real end;
-                  color = (red, white, blue);
-                  pp = ^ person;
-                  person = record age:      integer;
-                              friend:   pp;
-                              location: complex;
-                              favorite: color;
-                              salary:   real end;
-var c,d: complex; i, sum: integer;
-   ac: array[1..10] of complex;
-   aco: array[1..10, color] of color;
-   john, mary, fred, ptr: pp;
-   people: array[1..20] of person;
+var i,sum : integer;
 begin
-   ptr := ptr^.friend
+   i := i + 1;
+   sum := sum + 1;
+   i := i + sum
 end.
