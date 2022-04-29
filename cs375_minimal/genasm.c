6002a660478bdc42439a34a8640b223fcbbbfb9f
Lixun Zhang <zhanglx@utexas.edu>
2022-04-29 12:38:29 -0500

    [genasm] Change the year from Fall 2021 to Spring 2022

--- cs375_minimal/genasm.c
+++ cs375_minimal/genasm.c
@@ -121,7 +121,7 @@ char* bottomcodeb[] = {
   ""};
 
 char* bottomcodec[] = { 
-  "        .ident  \"CS 375 Compiler - Fall 2021\"",
+  "        .ident  \"CS 375 Compiler - Spring 2022\"",
   /* "        .section     .note.GNU-stack,\"\",@progbits", /* need this? */
   ""};
 
